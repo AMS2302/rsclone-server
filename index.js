5ba96fa692c75c642b1a535ecdd768ddaf51f79d
AMS2302 <ams23post@gmail.com>
2022-08-23 16:49:04 +0300

    create socket connection in index.js

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io'
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}...`);
 });
