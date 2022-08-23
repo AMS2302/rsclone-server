4e37ad486673603f50eca432a4b235a5d78a3c55
AMS2302 <ams23post@gmail.com>
2022-08-23 17:20:25 +0300

    docs: add comment

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import http from 'http';
-import { Server } from 'socket.io'
+import { Server } from 'socket.io';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+// * ================= testing if server is working ================
 
 io.on('connection', (socket) => {
     console.log('a user connected');
@@ -13,17 +15,6 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
     });
 });
-
-
-
-
-
-
-
-
-
-
-
 
 server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}...`);
