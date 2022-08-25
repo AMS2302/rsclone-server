5b21f11fed412ce328c7da26e271e17d7ff9f9ec
AMS2302 <ams23post@gmail.com>
2022-08-25 18:58:56 +0300

    feat: add cors in index.js

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3000;
+const ALLOWED_ORIGIN = `http://localhost:${PORT}`;
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+
+app.use(
+    cors({
+        origin: '*',
+        credentials: true,
+        optionSuccessStatus: 200,
+    })
+);
+
+const io = new Server(server, {
+    cors: ALLOWED_ORIGIN,
+    serveClient: false,
+});
 
 // * ================= testing if server is working ================
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('...a user connected');
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('...user disconnected');
     });
 });
 
 server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}...`);
 });
+
+// * ===============================================================
