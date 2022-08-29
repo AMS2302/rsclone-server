f9aed2619038a19b08841f265d9e057a183f6f31
AMS2302 <ams23post@gmail.com>
2022-08-30 00:43:34 +0300

    feat: add basic multiplayer functionality

--- scripts/multiplayer/multiplayer.js
+++ scripts/multiplayer/multiplayer.js
@@ -27,10 +27,27 @@ export default function multiplayer(socket) {
     connections[playerNumber] = false;
 
     socket.broadcast.emit('player-connection', `${playerNumber} connected`);
+    // todo: изменить второй параметр в зависимоти от реализации на клиенте
 
     socket.on('disconnect', () => {
         console.log(`Player ${playerNumber} disconnected`);
         connections[playerNumber] = null;
         socket.broadcast.emit('player-connection', `${playerNumber} disconnected`);
+    });
+
+    socket.on('player-ready', () => {
+        socket.broadcast.emit('enemy-ready', 'enemy is ready');
+        // todo: изменить второй параметр в зависимоти от реализации на клиенте
+        connections[playerNumber] = true;
+    });
+
+    socket.on('fire', (id) => {
+        console.log(`Shot fired from ${playerNumber}`, id);
+        socket.broadcast.emit('fire', id);
+    });
+
+    socket.on('fire-reply', (square) => {
+        console.log(square);
+        socket.broadcast.emit('fire-reply', square);
     });
 }
